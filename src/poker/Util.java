b8cc84546494a8d4416ba55e75345531888278aa
shaankeole <skeole24@brophybroncos.org>
2022-10-03 17:32:04 -0700

    scoring system finally finished

--- src/poker/Util.java
+++ src/poker/Util.java
@@ -2,11 +2,26 @@ package poker;
 
 import java.util.Arrays;
 
-public class Scoring {
-    
-    //public int suit; //0 = Diamonds, 1 = Hearts, 2 = Club, 3 = Spade
-    //public int number; //0 = 2, ..., 8 = 10, 9 = Jack, 10 = Queen, 11 = King, 12 = Ace
-    //2 of diamonds, 2 of hearts, 2 of club, 2 of spade, 3 of diamonds, ...
+public class Util {
+
+    static int size = 20;
+    static int[][] combinations = new int[size][size];
+    public static void init() {
+        for (int i = 0; i < size; i++) {
+            combinations[i][0] = 1;
+        }
+        for (int i = 1; i < size; i++) {
+            for (int j = 1; j < size; j++) {
+                combinations[i][j] = combinations[i - 1][j] + combinations[i - 1][j - 1];
+            }
+        }
+    }
+
+    /* 
+    Suit: 0 = Diamonds, 1 = Hearts, 2 = Club, 3 = Spade
+    Number: 0 = 2, ..., 8 = 10, 9 = Jack, 10 = Queen, 11 = King, 12 = Ace
+    Order: 2 of diamonds, 2 of hearts, 2 of club, 2 of spade, 3 of diamonds, ...
+    */
 
     public static int score(int[] deck) {
         //deck: the 5
@@ -35,19 +50,19 @@ public class Scoring {
 
         boolean two_pairs = false;
 
-        int first_pair = 0;
-        int second_pair = 0;
+        int first_pair = -1;
+        int second_pair = -1;
 
         for (int i = 0; i < 13; i++) {
             if ((cardValues[i] == 0) || (cardValues[i] == 1)) continue; //not sure if this is necessary
             if (cardValues[i] == 2) {
                 if (two_of_a_kind) two_pairs = true;
                 two_of_a_kind = true;
-                if (first_pair != 0) second_pair = i;
+                if (first_pair != -1) second_pair = i;
                 else first_pair = i;
             } else if (cardValues[i] == 3) {
                 three_of_a_kind = true;
-                if (first_pair != 0) {
+                if (first_pair != -1) {
                     second_pair = first_pair;
                     first_pair = i;
                 }
@@ -66,56 +81,69 @@ public class Scoring {
             values[4] = 3; //technically now it becomes 0 1 2 3 3 but idgaf
         } /* Ace, 2, 3, 4, 5 exception */
 
-        int score = 0;
+        for (int i = 0; i < 5; i++) {
+            if ((values[i] == first_pair) || (values[i] == second_pair)) {
+                values[i] = -1;
+            } else {
+                if (values[i] > first_pair) {
+                    values[i]--;
+                }
+                if (values[i] > second_pair) {
+                    values[i]--;
+                }
+            }
+        } 
+        Arrays.sort(values); //now, smth like 1 4 4 7 10 becomes -1 -1 1 6 9
+                            //2 2 8 8 9 becomes -1 -1 -1 -1 7
+                            
+        int index = 0;
+        int tiebreaker = 0;
+        if (straight) {
+            tiebreaker = values[4];
+        } else {
+            for (int i : values) {
+                if (i > -1) {
+                    index++;
+                    tiebreaker += combinations[i][index];
+                }
+            }
+        }
+        
+        //tiebreaker = final tiebreaker
+
+        if (flush) {
+            if (straight) {
+                return 7475 + tiebreaker;
+            } else {
+                return 5876 + tiebreaker;
+            }
+        } else {
+            if (four_of_a_kind) {
+                return 7319 + tiebreaker + 12 * first_pair;
+            } else if (full_house) {
+                if (second_pair > first_pair) second_pair--;
+                return 7163 + 12 * first_pair + second_pair;
+            } else if (straight) {
+                return 5863 + tiebreaker;
+            } else if (three_of_a_kind) {
+                return 5005 + tiebreaker + 66 * first_pair;
+            } else if (two_pairs) {
+                if (first_pair < second_pair) {
+                    int temp = second_pair;
+                    second_pair = first_pair;
+                    first_pair = temp;
+                } //make it so first pair > second pair
+                return 4147 + tiebreaker + 11 * (combinations[first_pair][2] + combinations[second_pair][1]);
+            } else if (two_of_a_kind) {
+                return 1287 + tiebreaker + 220 * first_pair;
+            } else {
+                return tiebreaker;
+            }
+        }
 
         /*
          * Nothing; 0
-            * Tiebreaker: High Card
-            * 13 choose 5 = 1287
-
-            * 0...12; 5 cards
-            it's increasing
-
-            we can make it 0...8 by subtracting (0, 1, 2, 3, 4) from the position
-                this makes it nondecreasing
-            now we want to order it
-
-            order: 
-            0 1 2 3 4
-            0 1 2 3 5
-            0 1 2 4 5
-            0 1 3 4 5
-            0 2 3 4 5
-            1 2 3 4 5
-            0 1 2 3 6
-
-            how to convert this into strictly increasing
-            reverse the order
-            4 3 2 1 0; 4 choose 4
-            5 3 2 1 0
-            5 4 2 1 0
-            5 4 3 1 0
-            5 4 3 2 0
-            5 4 3 2 1; 5 choose 4
-            6 3 2 1 0; we get 6 + 0 + 0 + 0 + 0 yep
-            note they're always strictly decreasing
-
-            ok so basically
-            # of ways for a number to start with digit k1: k choose 4
-            up to k1: 
-            4 choose 4 + 5 choose 4 + ... + (k-1) choose 4 = k choose 5
-            # of ways for it to start with k1 and then k2: k2 choose 3
-            etc. etc.
-            so basically our order is
-            k1 choose 5 + k2 choose 4 + k3 choose 3 + k4 choose 2 + k5 choose 1
-            perhaps we could do differences
-
-            12 11 10 9 8
-            SHOULD equal 1286 because 1287 total possibilities
-
-            12 choose 5 + 11 choose 4 + 10 choose 3 + 9 choose 2 + 8 choose 1
-
-            TL; DR; let's say the cards read (in decreasing order) k1, k2, k3, k4, k5
+            Let's say the cards read (in decreasing order) k1, k2, k3, k4, k5
             Then, the tiebreaker score is (k1 choose 5) + (k2 choose 4) + ... + (k5 choose 1)
             Where something "undefined" like 3 choose 4 = 0
 
@@ -132,6 +160,7 @@ public class Scoring {
             * Ace and 7 beats King and Queen
             * Ace and King beats Ace and Queen
             * 13 choose 2 * 11 = 858
+            * Maximum choose 2 + Minimum choose 1
             if > k2 subtract 2 and if between k1 and k2 subtract 1
 
          * 3 of a Kind; 5005
@@ -159,9 +188,6 @@ public class Scoring {
         
          * TOTAL: 7488
          */
-        
-        return score;
 
     }
-
 }
